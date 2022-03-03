d618ce43bd0f76d255d8848f25aa7879a51f366f
Nitila <100608921+nitilagokulkrishnan@users.noreply.github.com>
2022-03-03 16:45:08 +0530

    coursera project 1

---
+++ plot2.R
@@ -0,0 +1,16 @@
+## Getting full dataset
+sourcefile <- "household_power_consumption.txt"
+data <- read.table(sourcefile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+## Subsetting the data
+dateddata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+activepower <- as.numeric(dateddata$Global_active_power)
+png("plot2.png")
+plot(datetime, activepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
